e85050cb2d354378cbbb5a685a7af6398f1bf73d
kmargosb <cmargosb@gmail.com>
2023-11-13 21:28:41 +0100

    falta desectructurar y animaciones

--- main.js
+++ main.js
@@ -1,270 +1,5 @@
-const items = {
-  "weapons": [
-    {
-      "name": "Tree Branch",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Tree_Branch_Icon.png",
-      "value": "2",
-      "stats": {
-        "damage": 6
-      },
-      "isNew": true,
-      "description": "Wooden branches such as this are pretty common, but it's surprisingly well-balanced. It doesn't do much damage but can serve as a weapon in a pinch."
-    },
-    {
-      "name": "Ancient Short Sword",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Ancient_Short_Sword_Icon.png",
-      "value": "40",
-      "stats": {
-        "damage": 24
-      },
-      "description": "The blade of this sword was made using an ancient power lost to this modern age. Its blade appears only when drawn, and its cutting power surpasses metal swords."
-    },
-    {
-      "name": "Dragonbone Boko Club",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Dragonbone_Boko_Club_Icon.png",
-      "value": "24",
-      "stats": {
-        "damage": 20
-      },
-      "isNew": true,
-      "description": "This Bokoblin club has been reinforced with fossilized bones to maximize clobbering potential. Only the brawniest of Bokoblins can manage its immense weight."
-    },
-    {
-      "name": "Feathered Edge",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Feathered_Edge_Icon.png",
-      "value": "15",
-      "stats": {
-        "damage": 16
-      },
-      "description": "Rito craftsmen forged this lightweight, double-edge sword so Rito warriors could soar into battle unhindered by its weight."
-    },
-    {
-      "name": "Flameblade",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Flameblade_Icon.png",
-      "value": "24",
-      "stats": {
-        "damage": 26
-      },
-      "description": "This magical sword was forged in the lava of Death Mountain. It leaves white-hot flames in its wake when the blade glows red."
-    },
-    {
-      "name": "Soldier's Broadsword",
-      "category": "weapon",
-      "isNew": true,
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Soldier's_Broadsword_Icon.png",
-      "value": "14",
-      "stats": {
-        "damage": 14
-      },
-      "description": "A sword brandished by the soldiers who once protected Hyrule Castle. Its durable blade is well tuned for slaying monsters."
-    },
-    {
-      "name": "Royal Claymore",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Royal_Claymore_Icon.png",
-      "value": "52",
-      "stats": {
-        "damage": 32
-      },
-      "description": "A two-handed sword issued to the Hyrulean royal family's immediate guard detail. Its powerful strikes are said to crush an opponent's body and resolve alike."
-    },
-    {
-      "name": "Master Sword",
-      "category": "weapon",
-      "icon": "https://practise-game-inventory.vercel.app/items/weapons/BotW_Master_Sword_Icon.png",
-      "value": "30",
-      "stats": {
-        "damage": 28
-      },
-      "description": "The Legendary sword that seals the darkness. Its blade gleams with a sacred luster that can oppose the Calamity. Only a hero chosen by the sword itself may wield it."
-    }
-  ],
-  "shields": [
-    {
-      "name": "Wooden Shield",
-      "category": "shield",
-      "icon": "https://practise-game-inventory.vercel.app/items/shields/BotW_Wooden_Shield_Icon.png",
-      "value": "2",
-      "stats": {
-        "defense": 5
-      },
-      "description": "This lightweight, simple shield is ideal for less-experienced fighters. It can withstand light attacks, but blocking stronger blows is not recommended."
-    },
-    {
-      "name": "BotW Traveler's Shield",
-      "category": "shield",
-      "icon": "https://practise-game-inventory.vercel.app/items/shields/BotW_Traveler's_Shield_Icon.png",
-      "value": "4",
-      "stats": {
-        "defense": 7
-      },
-      "description": "A sturdy shield loved by many an adventurer. It is made of animal hide and sturdy wood and is best suited to defending against weak monsters or animals."
-    },
-    {
-      "name": "BotW Gerudo Shield",
-      "category": "shield",
-      "isNew": true,
-      "icon": "https://practise-game-inventory.vercel.app/items/shields/BotW_Gerudo_Shield_Icon.png",
-      "value": "20",
-      "stats": {
-        "defense": 12
-      },
-      "description": "The design of this metal shield has changed over time to match the Gerudo's sword-and-shield fighting style. It's favored by soldiers and travelers alike."
-    },
-    {
-      "name": "BotW Ancient Shield",
-      "category": "shield",
-      "icon": "https://practise-game-inventory.vercel.app/items/shields/BotW_Ancient_Shield_Icon.png",
-      "value": "70",
-      "stats": {
-        "defense": 25
-      },
-      "description": "This shield was made using ancient Sheikah technology. Its surface glows blue when raised in defense. Enhanced functionality allows it to deflect Guardian beams."
-    }
-  ],
-  "armors": [
-    {
-      "name": "Zora Helm",
-      "category": "helm",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Zora_Helm_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 5
-      },
-      "bonus": "swimming",
-      "description": "Zora headgear made from dragon scales. Increases swimming speed and allows you to use Spin Attack."
-    },
-    {
-      "name": "Zora Armor",
-      "category": "armor",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Zora_Armor_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 7
-      },
-      "bonus": "swimming",
-      "description": "Custom armor painstakingly crafted by each generation's Zora princess for her future husband. Wearing it will give you the ability to swim up waterfalls."
-    },
-    {
-      "name": "Zora Greaves",
-      "category": "greave",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Zora_Greaves_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 8
-      },
-      "bonus": "swimming",
-      "description": "These greave have been passed down among the Zora for generations. It's been said they're crafted using dragon scales. Equip them to swim faster."
-    },
-    {
-      "name": "Climber's Bandanna",
-      "category": "helm",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Climber's_Bandanna_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 4
-      },
-      "bonus": "climbing",
-      "description": "It may look like a regular bandanna, but it's actually infused with ancient technology that enhances core strength to improve your climbing ability."
-    },
-    {
-      "name": "Climbing Gear",
-      "category": "armor",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Climbing_Gear_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 9
-      },
-      "bonus": "climbing",
-      "description": "The ancient technology in this gear will make you a better climber. The special no-slip gloves help you use your energy more efficiently to facilitate nimble climbing."
-    },
-    {
-      "name": "Climbing Boots",
-      "category": "greave",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Climbing_Boots_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 3
-      },
-      "bonus": "climbing",
-      "description": "These rock-climbing boots have special no-slip toes that help you cling to walls. This ancient technology facilitates more nimble climbing."
-    },
-    {
-      "name": "Flamebreaker Helm",
-      "category": "helm",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Flamebreaker_Helm_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 12
-      },
-      "bonus": "fire",
-      "description": "Stone headgear made by Goron craftsmen to protect tourists visiting Goron City. As if its flame resistance isn't enough of a selling point, it also covers your entire head!"
-    },
-    {
-      "name": "Flamebreaker Armor",
-      "category": "armor",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Flamebreaker_Armor_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 18
-      },
-      "bonus": "fire",
-      "description": "Armor crafted by Gorons for Hylians curious enough to visit Goron City. It's made from fire-resistant rocks to protect the wearer."
-    },
-    {
-      "name": "Flamebreaker Boots",
-      "category": "greave",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Flamebreaker_Boots_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 15
-      },
-      "bonus": "fire",
-      "description": "Goron artisans used flame-resistant rocks to craft these fireproof boots for curious Hylians visiting Goron City."
-    },
-    {
-      "name": "Cap of the Hero",
-      "category": "helm",
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Cap_of_the_Hero_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 18
-      },
-      "bonus": "normal",
-      "description": "According to legend, this cap was once treasured by an ancient hero. It's quite the simple cap, yet there's something about it that's just so appealing..."
-    },
-    {
-      "name": "Champion's Tunic",
-      "category": "armor",
-      "isNew": true,
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Champion's_Tunic_Icon.png",
-      "value": "5",
-      "stats": {
-        "armor": 22
-      },
-      "bonus": "normal",
-      "description": "In ancient Hyrule, this garment could only be worn by one who had earned the respect of the royal family. Equipping it will reveal an enemy's life gauge."
-    },
-    {
-      "name": "Hylian Trousers",
-      "category": "greave",
-      "isNew": true,
-      "icon": "https://practise-game-inventory.vercel.app/items/armors/BotW_Hylian_Trousers_Icon.png",
-      "value": "3",
-      "stats": {
-        "armor": 14
-      },
-      "bonus": "normal",
-      "description": "Traditional dress trousers of Hyrule. The plush fabric makes these trousers quite comfortable, and their high durability makes them ideal for travelers."
-    }
-  ]
-}
-
+import { items } from './items.js'
+const body = document.querySelector('body')
 const stuff = document.querySelectorAll('.class-item');
 const itemCont = document.querySelectorAll('.item-cont');
 const statsCont = document.querySelectorAll('.statcont');
@@ -275,26 +10,33 @@ const statsInitial = document.querySelector('.statInitial > p');
 const statsFinal = document.querySelector('.statFinal > p');
 const type = document.querySelector('.type');
 const nombreStuff = document.querySelector('.right-bottom > h2');
+const equip = document.querySelector('.equip');
+const trow = document.querySelector('.trow');
+const cancel = document.querySelector('.cancel');
+const arma = document.querySelector('.arma');
+const pecho = document.querySelector('.pecho');
+const escudo = document.querySelector('.escudo');
+const cabeza = document.querySelector('.cabeza');
+const botas = document.querySelector('.botas')
 
-const weapons = items.weapons
-const shields = items.shields
-const armor = items.armors
+const {weapons, shields, armors} = items
+
 
 let weaponName = weapons.map(weapon => weapon.name)
 let shieldName = shields.map(shield => shield.name)
-let armorName = armor.map(armadura => armadura.name)
+let armorName = armors.map(armadura => armadura.name)
 
 let weaponIcon = weapons.map(weapon => weapon.icon)
 let shieldIcon = shields.map(shield => shield.icon)
-let armourIcon = armor.map(armadura => armadura.icon)
+let armourIcon = armors.map(armadura => armadura.icon)
 
 let weaponStats = weapons.map(weapon => weapon.stats.damage)
 let shieldStats = shields.map(shield => shield.stats.defense)
-let armourStats = armor.map(armadura => armadura.stats.armor)
+let armourStats = armors.map(armadura => armadura.stats.armor)
 
 let weaponDescription = weapons.map(weapon => weapon.description)
 let shieldDescription = shields.map(shield => shield.description)
-let armorDescription = armor.map(armadura => armadura.description)
+let armorDescription = armors.map(armadura => armadura.description)
 
 //Botones Stuff (muestra las armas escudos y armaduras)
 stuff.forEach((item, i) => {
@@ -314,24 +56,26 @@ stuff.forEach((item, i) => {
         if (i < weaponIcon.length) {
           cont.style.backgroundColor = "transparent"
           cont.style.backgroundImage = `url("${weaponIcon[i]}")`
+          cont.id = weaponName[i];
         }
       })
     }
     else if (item.classList[1] == "shield") {
       statsCont.forEach((stat, i) => {
         if (i < shieldStats.length) {
-          stat.style.opacity = "1"
-          stat.innerText = `${shieldStats[i]}`
+          stat.style.opacity = "1";
+          stat.innerText = `${shieldStats[i]}`;
         } else {
-          stat.style.opacity = "0"
+          stat.style.opacity = "0";
         }
       })
       itemCont.forEach((cont, i) => {
-        cont.style.backgroundImage = "none"
-        cont.style.backgroundColor = "#1E293B"
+        cont.style.backgroundImage = "none";
+        cont.style.backgroundColor = "#1E293B";
         if (i < shieldIcon.length) {
-          cont.style.backgroundColor = "transparent"
-          cont.style.backgroundImage = `url("${shieldIcon[i]}")`
+          cont.style.backgroundColor = "transparent";
+          cont.style.backgroundImage = `url("${shieldIcon[i]}")`;
+          cont.id = shieldName[i];
         }
       })
     }
@@ -349,18 +93,64 @@ stuff.forEach((item, i) => {
           cont.style.background = "none"
           cont.style.backgroundColor = "transparent"
           cont.style.backgroundImage = `url("${armourIcon[i]}")`
+          cont.id = armorName[i]
         }
       })
     }
   })
 })
 
-//Selecciona cada elemento muestra su descripcion y sus stats + menu de agregar o remover
+//Selecciona cada elemento muestra su descripcion y sus stats + Menu de agregar o remover
 itemCont.forEach((cont, i) => {
-
   cont.addEventListener('click', (e) => {
 
-    if ((cont.style.backgroundImage).includes("weapon")) {
+    const espada = (cont.style.backgroundImage).includes("weapon");
+    const escudex = (cont.style.backgroundImage).includes("shield");
+    const ropa = (cont.style.backgroundImage).includes("armors");
+
+    equip.addEventListener('click', (even) => {
+      if (espada) {
+        menu.classList.remove('activo')
+        arma.style.backgroundImage = 'none'
+        arma.style.backgroundImage = `url("${weaponIcon[i]}")`
+      }
+      else if (escudex) {
+        menu.classList.remove('activo')
+        escudo.style.backgroundImage = 'none'
+        escudo.style.backgroundImage = `url("${shieldIcon[i]}")`
+      }
+      else if (ropa) {
+        menu.classList.remove('activo')
+        armors.forEach(pieza => {
+          if (pieza.name == e.target.id) {
+            switch (pieza.category) {
+              case 'helm':
+                cabeza.style.backgroundImage = 'none'
+                cabeza.style.backgroundImage = `url("${pieza.icon}")`
+                break;
+              case 'greave':
+                botas.style.backgroundImage = 'none'
+                botas.style.backgroundImage = `url("${pieza.icon}")`
+                break;
+              case 'armor':
+                pecho.style.backgroundImage = 'none'
+                pecho.style.backgroundImage = `url("${pieza.icon}")`
+            }
+          }
+        })
+      }
+    })
+
+    if (espada || escudex || ropa) {
+      menu.classList.toggle('activo')
+      menu.style.opacity = "1"
+      menu.style.left = `${e.clientX}px`
+      menu.style.top = `${e.clientY}px`
+    } else {
+      menu.classList.remove('activo')
+    }
+
+    if (espada) {
       rightBottom.style.opacity = "1"
       nombreStuff.innerText = `${weaponName[i]}`
       description.innerText = `${weaponDescription[i]}`
@@ -374,7 +164,7 @@ itemCont.forEach((cont, i) => {
           ? (statsInitial.style.border = "1px solid red") && (statsFinal.style.border = "1px solid green")
           : (statsInitial.style.border = "1px solid green") && (statsFinal.style.border = "1px solid green")
     }
-    else if ((cont.style.backgroundImage).includes("shield")) {
+    else if (escudex) {
 
       type.classList.remove("weap", "shie", "arm")
       rightBottom.style.opacity = "1"
@@ -389,7 +179,7 @@ itemCont.forEach((cont, i) => {
           ? (statsInitial.style.border = "1px solid red") && (statsFinal.style.border = "1px solid green")
           : (statsInitial.style.border = "1px solid green") && (statsFinal.style.border = "1px solid green")
 
-    } else if ((cont.style.backgroundImage).includes("armors")) {
+    } else if (ropa) {
 
       type.classList.remove("weap", "shie", "arm")
       rightBottom.style.opacity = "1"
@@ -405,36 +195,17 @@ itemCont.forEach((cont, i) => {
           : (statsInitial.style.border = "1px solid green") && (statsFinal.style.border = "1px solid green")
     }
     else { rightBottom.style.opacity = "0" }
-  })
-})
-
-
-// cont.classList.add("itemCont-focus")
-// cont.classList.remove("itemCont-focus")
-// e.target.classList.add("itemCont-focus") 
-
-
-itemCont.forEach((cont,i) =>{
-  cont.addEventListener('click', (e)=>{
-    // console.log(e.clientX, e.clientY)
-    menu.classList.toggle('activo')
-    menu.style.opacity = "1"
-    menu.style.left = `${e.clientX}px`
-    menu.style.top = `${e.clientY}px`
-
-    console.log(menu.style.top, e.clientX)
-    
 
   })
 })
 
 
+trow.addEventListener('click', (e) => {
+})
+cancel.addEventListener('click', () => {
+  menu.classList.remove('activo')
+})
 
 
-
-
-
-
-
-
-
+// cont.classList.remove('itemCont-focus')
+// e.target.classList.add('itemCont-focus')
